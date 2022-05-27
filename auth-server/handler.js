2019c7c435adf6f82de59bb5cd24338a455cc496
Kevin Daysal <kdaysal123@gmail.com>
2022-05-27 15:19:01 -0400

    Added headers to allow all origins (to fix CORS error in console)

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -93,6 +93,9 @@ module.exports.getAccessToken = async (event) => {
       // Respond with OAuth token 
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
         body: JSON.stringify(token),
       };
     })
