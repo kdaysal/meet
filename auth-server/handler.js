76e8e81ddad1cbe27274e9ecfe1f126e57b4318b
Kevin Daysal <kdaysal123@gmail.com>
2022-05-31 09:58:43 -0400

    Created new async function 'getCalendarEvents'

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -113,3 +113,60 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+module.exports.getCalendarEvents = event => {
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({ events: results.data.items })
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Credentials": true,
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(err),
+      };
+    });
+
+}
