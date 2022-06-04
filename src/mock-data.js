5cfa346518da1ba095e175df9aae272621850802
Kevin Daysal <kdaysal123@gmail.com>
2022-06-04 11:05:08 -0400

    Created a new mockData array to begin writing tests related to calendar event details

---
+++ src/mock-data.js
@@ -0,0 +1,142 @@
+const mockData = [
+  {
+    "kind": "calendar#event",
+    "etag": "\"3187221506816000\"",
+    "id": "48m09le389vijbqfs2mcr6i87q_20200630T070000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NDhtMDlsZTM4OXZpamJxZnMybWNyNmk4N3FfMjAyMDA2MzBUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-07-01T13:39:01.000Z",
+    "updated": "2020-07-01T13:39:13.408Z",
+    "summary": "Node Gang",
+    "description": "We meet every Tuesday to talk about Node or JavaScript in general. Node Gang is an inclusive community that tries to cater to all levels of learners during meetups. Join us if you are curious to hear about what's new in Node, patterns, interesting npm packages, and practices.",
+    "location": "Sydney NSW, Australia",
+    "creator": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "organizer": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "start": {
+      "dateTime": "2020-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "end": {
+      "dateTime": "2020-06-30T10:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "48m09le389vijbqfs2mcr6i87q",
+    "originalStartTime": {
+      "dateTime": "2020-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "48m09le389vijbqfs2mcr6i87q@google.com",
+    "sequence": 1,
+    "hangoutLink": "https://meet.google.com/wdc-wutt-tji",
+    "conferenceData": {
+      "entryPoints": [
+        {
+          "entryPointType": "video",
+          "uri": "https://meet.google.com/wdc-wutt-tji",
+          "label": "meet.google.com/wdc-wutt-tji"
+        },
+        {
+          "entryPointType": "more",
+          "uri": "https://tel.meet/wdc-wutt-tji?pin=3796108774207",
+          "pin": "3796108774207"
+        },
+        {
+          "regionCode": "DE",
+          "entryPointType": "phone",
+          "uri": "tel:+49-40-8081618974",
+          "label": "+49 40 8081618974",
+          "pin": "170634768"
+        }
+      ],
+      "conferenceSolution": {
+        "key": {
+          "type": "hangoutsMeet"
+        },
+        "name": "Google Meet",
+        "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+      },
+      "conferenceId": "wdc-wutt-tji"
+    },
+    "reminders": {
+      "useDefault": true
+    },
+    "eventType": "default"
+  },
+  {
+    "kind": "calendar#event",
+    "etag": "\"3187232532588000\"",
+    "id": "20c51je3q99mnolr1rp06dt9jd_20200630T120000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=MjBjNTFqZTNxOTltbm9scjFycDA2ZHQ5amRfMjAyMDA2MzBUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-07-01T14:17:12.000Z",
+    "updated": "2020-07-01T15:11:06.294Z",
+    "summary": "Use jQuery, bring in interactivity easily ",
+    "description": "Do you know jQuery is used by around 70 percent of the 10 million most popular websites as of May 2019? \n\nThough many consider it dead after Angular and Express gained popularity, jQuery is still an important part of many websites. \n\nIn our workshop, we teach basic to advanced jQuery where you will also be able to build a simple app using it. If you are familiar with JS, join us to learn probably its most popular library.",
+    "location": "Mumbai, Maharashtra, India",
+    "creator": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "organizer": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "start": {
+      "dateTime": "2020-06-30T14:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "end": {
+      "dateTime": "2020-06-30T15:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "20c51je3q99mnolr1rp06dt9jd",
+    "originalStartTime": {
+      "dateTime": "2020-06-30T14:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "20c51je3q99mnolr1rp06dt9jd@google.com",
+    "sequence": 1,
+    "hangoutLink": "https://meet.google.com/nns-htdr-ijk",
+    "conferenceData": {
+      "entryPoints": [
+        {
+          "entryPointType": "video",
+          "uri": "https://meet.google.com/nns-htdr-ijk",
+          "label": "meet.google.com/nns-htdr-ijk"
+        },
+        {
+          "entryPointType": "more",
+          "uri": "https://tel.meet/nns-htdr-ijk?pin=9302146320475",
+          "pin": "9302146320475"
+        },
+        {
+          "regionCode": "DE",
+          "entryPointType": "phone",
+          "uri": "tel:+49-40-8081618430",
+          "label": "+49 40 8081618430",
+          "pin": "810644618"
+        }
+      ],
+      "conferenceSolution": {
+        "key": {
+          "type": "hangoutsMeet"
+        },
+        "name": "Google Meet",
+        "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+      },
+      "conferenceId": "nns-htdr-ijk"
+    },
+    "reminders": {
+      "useDefault": true
+    },
+    "eventType": "default"
+  }
+];//end mockData array
+
+export { mockData };
