74f118fc51d7d6bf6e46d676d0aba6932ae2c50d
Kevin Daysal <kdaysal123@gmail.com>
2022-06-04 12:13:35 -0400

    Wrote test to verify that the list of suggestions rendered matches the list of suggestions in the component state

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
+import { mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
+  let CitySearchWrapper;
+  beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
+
   //This test checks whether an element with the class name 'city' exists within the CitySearchWrapper component
   test('render text input', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
@@ -10,26 +17,35 @@ describe('<CitySearch /> component', () => {
   });
 
   test('renders a list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 
   test('renders text input correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
-
     //create a new variable 'query' and assign it to the value that the user types into the textbox
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
   test('change state when text input changes', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper.setState({
       query: 'Munich'
     });
     const eventObject = { target: { value: 'Berlin' } };
     CitySearchWrapper.find('.city').simulate('change', eventObject);
     expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  });
+
+  //this test verifies that the list of suggestions rendered matches the list of suggestions in the component state
+  test('render list of suggestions correctly', () => {
+    const locations = extractLocations(mockData);
+    CitySearchWrapper.setState({ suggestions: locations });
+    const suggestions = CitySearchWrapper.state('suggestions');
+
+    //the "+ 1" after "suggestions.length" is because I will be manually adding a "See all cities" suggestion at the end of the suggestions list
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+    }
   });
 
 });//end describe
