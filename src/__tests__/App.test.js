7b3eab9c58e59dfaa89432303e4cd0304e845ad8
Kevin Daysal <kdaysal123@gmail.com>
2022-06-04 10:23:17 -0400

    Refactored to use beforeAll() to shallow render the App component before all tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 //'<App /> component' is a new 'scope'
 //renders the 'App' component using the shallow rendering API
-//this test ensures that there exists only one EventList component within the App component
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+  //this test ensures that there exists only one EventList component within the App component
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
-});
+
+  //similarly, this test ensures that the required component, CitySearch, exists
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
+});//end describe
