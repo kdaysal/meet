d1dc9d1c77a55bcade22b988e6bd2724530b9d43
Kevin Daysal <kdaysal123@gmail.com>
2022-06-05 13:55:38 -0400

    Refactored tests to declare shallow rendering of the component one time in a beforeAll() function rather than in each individual unit test.

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -5,42 +5,40 @@ import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
 
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]} />);
+  });
+
   //most basic of all the tests - check if a single event is rendered
   test('render a single event (1st element of mockData)', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find(".event")).toHaveLength(1);
   });
 
   test('render the summary of the event', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find(".summary")).toHaveLength(1);
   })
 
   test('render the start date and time zone for the event', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find(".start-date-time")).toHaveLength(1);
   })
 
   test('render the location for the event', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find(".location")).toHaveLength(1);
   })
 
   //just test whether the more-details-button actually renders
   test("render the show details button", () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find(".more-details-button")).toHaveLength(1);
   });
 
   //check whether state of 'collapsed' is set to 'true' when the Event component is first rendered
   test('event details are collapsed by default upon initial render', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.state('collapsed')).toBe(true);
   });
 
   //check whether simulating a click on show-details button changes state of 'collapsed' to false
   test("clicking show-details button changes state of 'collapsed' to 'false'", () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     EventWrapper.setState({ collapsed: true });
     EventWrapper.find('.more-details-button').simulate('click');
     expect(EventWrapper.state('collapsed')).toBe(false);
@@ -48,7 +46,6 @@ describe('<Event /> component', () => {
 
   //check whether simulating a click on hide-details button changes state of 'collapsed' to true
   test("clicking hide-details button changes state of 'collapsed' to 'true'", () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     EventWrapper.setState({ collapsed: false });
     EventWrapper.find('.more-details-button').simulate('click');
     expect(EventWrapper.state('collapsed')).toBe(true);
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,22 +4,24 @@ import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
 
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />)
+  });
+
   //test that the NumberOfEvents input field actually renders
   test('render text input field', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.number-events-input')).toHaveLength(1);
   });
 
   //test whether setting the state of numberOfEvents manually (via code, not user-input) updates the state
   test('check whether state of numberOfEvents can be updated (manually)', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '15' });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual("15");
   });
 
   //simulate a change (within acceptable range) to numberOfEvents via user input
   test('update state of numberOfEvents when the user changes the input', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '32' });
     NumberOfEventsWrapper.find('.number-events-input').simulate('change', { target: { value: "5" } });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual("5");
@@ -27,7 +29,6 @@ describe('<NumberOfEvents /> component', () => {
 
   //testing the message - simulate a change (below acceptable range) to numberOfEvents via user input
   test('update state of message if numberOfEvents is below range', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '32' });
     NumberOfEventsWrapper.find('.number-events-input').simulate('change', { target: { value: "0" } });
     expect(NumberOfEventsWrapper.state('message')).toEqual('Out of range - please enter a number between 1 and 32');
@@ -35,7 +36,6 @@ describe('<NumberOfEvents /> component', () => {
 
   //testing the message - simulate a change (above acceptable range) to numberOfEvents via user input
   test('update state of message if numberOfEvents is above range', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '32' });
     NumberOfEventsWrapper.find('.number-events-input').simulate('change', { target: { value: "33" } });
     expect(NumberOfEventsWrapper.state('message')).toEqual('Out of range - please enter a number between 1 and 32');
@@ -43,7 +43,6 @@ describe('<NumberOfEvents /> component', () => {
 
   //testing the state of numberOfEvents - simulate a change (below acceptable range) to numberOfEvents via user input
   test('if numberOfEvents is below range, state of numberOfEvents is unchanged', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '32' });
     NumberOfEventsWrapper.find('.number-events-input').simulate('change', { target: { value: "0" } });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual('32');
@@ -51,7 +50,6 @@ describe('<NumberOfEvents /> component', () => {
 
   //testing the state of numberOfEvents - simulate a change (above acceptable range) to numberOfEvents via user input
   test('if numberOfEvents is above range, state of numberOfEvents is unchanged', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     NumberOfEventsWrapper.setState({ numberOfEvents: '32' });
     NumberOfEventsWrapper.find('.number-events-input').simulate('change', { target: { value: "33" } });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual('32');
