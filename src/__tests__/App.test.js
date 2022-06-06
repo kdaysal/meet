6fcc1c90ca3a2a3b0f86ae960328d176ed2fb43b
Kevin Daysal <kdaysal123@gmail.com>
2022-06-05 21:55:41 -0400

    Added test to check if getting the list of events matching the city selected by the user is successful
    -Imported mockData, extractLocations, and getEvents

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,8 @@ import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberOfEvents';
+import { mockData } from '../mock-data';
+import { extractLocations, getEvents } from '../api';
 
 //UNIT tests
 //'<App /> component' is a new 'scope'
@@ -45,6 +47,21 @@ describe('<App /> integration', () => {
     const AppLocationsState = AppWrapper.state('locations');
     expect(AppLocationsState).not.toEqual(undefined);
     expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+    AppWrapper.unmount();
+  });
+
+  test('get list of events matching the city selected by the user', async () => {
+    const AppWrapper = mount(<App />);
+    const CitySearchWrapper = AppWrapper.find(CitySearch);
+    const locations = extractLocations(mockData);
+    CitySearchWrapper.setState({ suggestions: locations });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    const selectedIndex = Math.floor(Math.random() * (suggestions.length));
+    const selectedCity = suggestions[selectedIndex];
+    await CitySearchWrapper.instance().handleItemClicked(selectedCity);
+    const allEvents = await getEvents();
+    const eventsToShow = allEvents.filter(event => event.location === selectedCity);
+    expect(AppWrapper.state('events')).toEqual(eventsToShow);
     AppWrapper.unmount();
   });
 
