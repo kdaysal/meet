3ffbe1d8f0a4b8e5d81f30acd2dfe02c4c02231e
Kevin Daysal <kdaysal123@gmail.com>
2022-06-05 22:19:37 -0400

    Modified getEvents to render accordingly based on whether "Show all cities" OR a specific city was selected
    Added componentdidMount and componentWillUnmount functions to update the state only if the component is actually mounted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
 
 class App extends Component {
 
@@ -13,9 +13,25 @@ class App extends Component {
     locations: []
   }
 
+  //using this.mounted boolean to update the state only if the component is mounted
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => event.location === location);
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents
       });
