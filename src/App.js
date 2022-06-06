68ecbb7f39e643edd72db8955d07134f90b6bf83
Kevin Daysal <kdaysal123@gmail.com>
2022-06-05 21:21:07 -0400

    Added new states to App.js for 'events' and 'locations', then passed them as props to their respective components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+
+  //initialize states to empty/default
+  state = {
+    events: [],
+    locations: []
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
