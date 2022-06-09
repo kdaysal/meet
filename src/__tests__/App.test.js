b15d314286ed9d98f98207c41832c65c44361a6e
Kevin Daysal <kdaysal123@gmail.com>
2022-06-08 23:34:05 -0400

    Added test to check if App passes "numberOfEvents" state as a prop to NumberOfEvents component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -50,6 +50,14 @@ describe('<App /> integration', () => {
     AppWrapper.unmount();
   });
 
+  test('App passes "numberOfEvents" state as a prop to NumberOfEvents', () => {
+    const AppWrapper = mount(<App />);
+    const AppNumberOfEvents = AppWrapper.state('numberOfEvents');
+    expect(AppNumberOfEvents).not.toEqual(undefined);
+    expect(AppWrapper.find(NumberOfEvents).props().numberOfEvents).toEqual(AppNumberOfEvents);
+    AppWrapper.unmount();
+  });
+
   test('get list of events matching the city selected by the user', async () => {
     const AppWrapper = mount(<App />);
     const CitySearchWrapper = AppWrapper.find(CitySearch);
