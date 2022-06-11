6d160799e38c996fc11049a204faf83313b02dd3
Kevin Daysal <kdaysal123@gmail.com>
2022-06-11 11:46:25 -0400

    Corrected syntax errors from previous refactoring of my EndToEnd test suite
    -I added the beforeAll() and afterAll() methods to launch/close the testing browser, but forgot to remove the same redundant code from each of my tests :)

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -11,40 +11,23 @@ describe('show/hide an event details', () => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
+    await page.waitForSelector('.event'); //waitForSelector is an API method that will wait for the Event component to be loaded first
+  });
+
+  //Scenario 1
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .show-more-details');// note - 'page.$(selector)' will return the first CSS selector found with the given name
+    expect(eventDetails).toBeNull();
+  });
+
+  //Scenario 2
+  test('User can expand an event to see its details', async () => {
+    const eventDetails = await page.$('.event .show-more-details');
+    expect(eventDetails).toBeDefined();// check if the .show-more-details className exists (i.e. that more details are indeed showing)
   });
 
   afterAll(() => {
     browser.close();
   });
-
-  //Scenario 1
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event');//waitForSelector is an API method that will wait for the Event component to be loaded first
-
-    const eventDetails = await page.$('.event .show-more-details');// note - 'page.$(selector)' will return the first CSS selector found with the given name
-    expect(eventDetails).toBeNull();
-    browser.close();
-  });
-
-  //Scenario 2
-  test('User can expand an event to see its details', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event');
-    await page.click('.event .more-details-button');
-
-    const eventDetails = await page.$('.event .show-more-details');
-    expect(eventDetails).toBeDefined();// check if the .show-more-details className exists (i.e. that more details are indeed showing)
-    browser.close();
-  });
-
 
 });//end describe
