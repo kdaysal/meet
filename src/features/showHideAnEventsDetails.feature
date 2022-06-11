34e41a00d6e2ac10c985bbab0b007b1d489dcca2
Kevin Daysal <kdaysal123@gmail.com>
2022-06-11 13:37:17 -0400

    Followed same procedure to create new .feature and .test.js files to test showHideAnEventsDetails functionality
    Ran tests with an intentionally blank 'defineFeature()' method and copied cucumber's suggested code formatting (from the error output after running the test) over to my showHideAnEventsDetails.test.js file to use as a template.

---
+++ src/features/showHideAnEventsDetails.feature
@@ -0,0 +1,17 @@
+Feature: Show / Hide an Event's Details
+
+  Scenario: An event element is collapsed by default.
+    Given the user is on the main page
+    When  the user has not clicked on an event element
+    Then the event element is collapsed by default
+
+  Scenario: User can expand an event to see its details
+    Given the user is on the main page
+    When the user clicks the Show Details button
+    Then the event element expands to reveal more details
+
+  Scenario:  User can collapse an event to hide its details
+    Given the user is on the main page
+    And an event element is currently expanded to show more details
+    When the user clicks the Hide Details button
+    Then the event element should collapse to hide the extra details
---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,63 @@
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from '../App';
+import { mockData } from '../mock-data';
+import CitySearch from '../CitySearch';
+import { extractLocations } from '../api';
+
+const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+const locations = extractLocations(mockData);
+
+defineFeature(feature, test => {
+
+  //Scenario 1
+  test('An event element is collapsed by default.', ({ given, when, then }) => {
+    given('the user is on the main page', () => {
+
+    });
+
+    when('the user has not clicked on an event element', () => {
+
+    });
+
+    then('the event element is collapsed by default', () => {
+
+    });
+  });
+
+  //Scenario 2
+  test('User can expand an event to see its details', ({ given, when, then }) => {
+    given('the user is on the main page', () => {
+
+    });
+
+    when('the user clicks the Show Details button', () => {
+
+    });
+
+    then('the event element expands to reveal more details', () => {
+
+    });
+  });
+
+  //Scenario 3
+  test('User can collapse an event to hide its details', ({ given, and, when, then }) => {
+    given('the user is on the main page', () => {
+
+    });
+
+    and('an event element is currently expanded to show more details', () => {
+
+    });
+
+    when('the user clicks the Hide Details button', () => {
+
+    });
+
+    then('the event element should collapse to hide the extra details', () => {
+
+    });
+  });
+
+});
