069719f7cc9e7167e54a25cbe268819895a1c22d
Kevin Daysal <kdaysal123@gmail.com>
2022-06-11 11:16:29 -0400

    Implemented test scenario 2 to check that a user can expand an event to see its details
    -Also refactored code to make puppeteer open/close the browsers only once instead of during each test (via beforeAll and afterAll)

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,10 +4,21 @@ import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
 
+  let browser;
+  let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
   });
 
+  afterAll(() => {
+    browser.close();
+  });
+
+  //Scenario 1
   test('An event element is collapsed by default', async () => {
     const browser = await puppeteer.launch();
 
@@ -16,8 +27,22 @@ describe('show/hide an event details', () => {
 
     await page.waitForSelector('.event');//waitForSelector is an API method that will wait for the Event component to be loaded first
 
-    const eventDetails = await page.$('.event .event__Details');// note - 'page.$(selector)' will return the first CSS selector found with the given name
+    const eventDetails = await page.$('.event .show-more-details');// note - 'page.$(selector)' will return the first CSS selector found with the given name
     expect(eventDetails).toBeNull();
+    browser.close();
+  });
+
+  //Scenario 2
+  test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+    await page.click('.event .more-details-button');
+
+    const eventDetails = await page.$('.event .show-more-details');
+    expect(eventDetails).toBeDefined();// check if the .show-more-details className exists (i.e. that more details are indeed showing)
     browser.close();
   });
 
