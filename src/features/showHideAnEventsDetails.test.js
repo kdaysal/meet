515f4a0be344a4ce1142b5d43220123738f5445b
Kevin Daysal <kdaysal123@gmail.com>
2022-06-11 14:23:22 -0400

    Implemented tests for the 3 scenarios of this feature
    -Also modified the Scenario 1 test to be an async function so I can be sure AppWrapper is mounted and is updated before proceeding.

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,13 +1,11 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
-import CitySearch from '../CitySearch';
 import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
-const locations = extractLocations(mockData);
 
 defineFeature(feature, test => {
 
@@ -15,9 +13,10 @@ defineFeature(feature, test => {
   //Scenario 1
   test('An event element is collapsed by default.', ({ given, when, then }) => {
 
-    given('the user is on the main page', () => {
+    given('the user is on the main page', async () => {
       //simply mount the App component to meet this criteria
-      AppWrapper = mount(<App />);
+      AppWrapper = await mount(<App />);
+      AppWrapper.update();
     });
 
     when('the user has not clicked on an event element', () => {
@@ -31,35 +30,39 @@ defineFeature(feature, test => {
 
   //Scenario 2
   test('User can expand an event to see its details', ({ given, when, then }) => {
-    given('the user is on the main page', () => {
-
+    given('1 or more events is displayed on the page', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.update();
+      expect(AppWrapper.find('.event')).toBeDefined();
     });
 
     when('the user clicks the Show Details button', () => {
-
+      AppWrapper.find('.more-details-button').at(0).simulate('click');
     });
 
     then('the event element expands to reveal more details', () => {
-
+      expect(AppWrapper.find('.show-more-details')).toHaveLength(1);
     });
   });
 
   //Scenario 3
   test('User can collapse an event to hide its details', ({ given, and, when, then }) => {
-    given('the user is on the main page', () => {
-
+    given('the user is on the main page', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.update();
     });
 
     and('an event element is currently expanded to show more details', () => {
-
+      AppWrapper.find('.more-details-button').at(0).simulate('click');
+      expect(AppWrapper.find('.show-more-details')).toHaveLength(1);
     });
 
     when('the user clicks the Hide Details button', () => {
-
+      AppWrapper.find('.more-details-button').at(0).simulate('click');
     });
 
     then('the event element should collapse to hide the extra details', () => {
-
+      expect(AppWrapper.find('.show-more-details')).toHaveLength(0);
     });
   });
 
