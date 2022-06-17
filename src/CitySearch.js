b8cb101300b04485bc88c0eea524b961e5f910c3
Kevin Daysal <kdaysal123@gmail.com>
2022-06-16 20:35:54 -0400

    Updated handleInputchanged function to update the state of 'infoText' if no suggestions are returned
    -Also added 'InfoAlert' component to the render() statement

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   //initialize state to an empty string
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: ''
   }
 
   //when a user starts typing in the City box, update the state of 'query' and 'suggestions'
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We cannot find the city you are looking for. Please try another city',
+      });
+    } else {
+      this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   //when a user clicks on a suggestion, update the state of 'query'
@@ -32,6 +43,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
@@ -40,6 +52,7 @@ class CitySearch extends Component {
           placeholder="Search for a city"
           onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
+
         <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
           {this.state.suggestions.map((suggestion) => (
             <li
