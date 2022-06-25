31c0f816a1d498fe9e8691c528b446990524a07f
Kevin Daysal <kdaysal123@gmail.com>
2022-06-25 11:54:23 -0400

    Added conditional to only show labels of genres if (percent > 0)
    Previously the labels of genres with counts of '0' were overlapping other genres in the chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -44,7 +44,12 @@ const EventGenre = ({ events }) => {
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+          label={({ name, percent }) => {
+            if (percent > 0) {
+              return `${name} ${(percent * 100).toFixed(0)}%`
+            }
+          }
+          }
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
