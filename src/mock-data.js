1754413bbd9e92f096c3ee4bf2a20be679b60c06
Kevin Daysal <kdaysal123@gmail.com>
2022-07-04 17:03:58 -0400

    Added more events to mock data for debugging

--- src/mock-data.js
+++ src/mock-data.js
@@ -175,6 +175,144 @@ const mockData = [
       "useDefault": true
     },
     "eventType": "default"
+  },
+  {
+    "kind": "calendar#event",
+    "etag": "\"3187221506816000\"",
+    "id": "98m09le389vijbqfs2mcr6i87q_20200630T070000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NDhtMDlsZTM4OXZpamJxZnMybWNyNmk4N3FfMjAyMDA2MzBUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-07-01T13:39:01.000Z",
+    "updated": "2020-07-01T13:39:13.408Z",
+    "summary": "jQuery stuff and things",
+    "description": "We meet every Tuesday to talk about Node or JavaScript in general. Node Gang is an inclusive community that tries to cater to all levels of learners during meetups. Join us if you are curious to hear about what's new in Node, patterns, interesting npm packages, and practices.",
+    "location": "Sydney NSW, Australia",
+    "creator": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "organizer": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "start": {
+      "dateTime": "2022-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "end": {
+      "dateTime": "2022-06-30T10:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "48m09le389vijbqfs2mcr6i87q",
+    "originalStartTime": {
+      "dateTime": "2022-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "48m09le389vijbqfs2mcr6i87q@google.com",
+    "sequence": 1,
+    "hangoutLink": "https://meet.google.com/wdc-wutt-tji",
+    "conferenceData": {
+      "entryPoints": [
+        {
+          "entryPointType": "video",
+          "uri": "https://meet.google.com/wdc-wutt-tji",
+          "label": "meet.google.com/wdc-wutt-tji"
+        },
+        {
+          "entryPointType": "more",
+          "uri": "https://tel.meet/wdc-wutt-tji?pin=3796108774207",
+          "pin": "3796108774207"
+        },
+        {
+          "regionCode": "DE",
+          "entryPointType": "phone",
+          "uri": "tel:+49-40-8081618974",
+          "label": "+49 40 8081618974",
+          "pin": "170634768"
+        }
+      ],
+      "conferenceSolution": {
+        "key": {
+          "type": "hangoutsMeet"
+        },
+        "name": "Google Meet",
+        "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+      },
+      "conferenceId": "wdc-wutt-tji"
+    },
+    "reminders": {
+      "useDefault": true
+    },
+    "eventType": "default"
+  },
+  {
+    "kind": "calendar#event",
+    "etag": "\"3187221506816000\"",
+    "id": "98m01le389vijbqfs2mcr6i87q_20200630T070000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NDhtMDlsZTM4OXZpamJxZnMybWNyNmk4N3FfMjAyMDA2MzBUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-07-01T13:39:01.000Z",
+    "updated": "2020-07-01T13:39:13.408Z",
+    "summary": "things about AngularJS-Remote things",
+    "description": "We meet every Tuesday to talk about Node or JavaScript in general. Node Gang is an inclusive community that tries to cater to all levels of learners during meetups. Join us if you are curious to hear about what's new in Node, patterns, interesting npm packages, and practices.",
+    "location": "Sydney NSW, Australia",
+    "creator": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "organizer": {
+      "email": "fullstackwebdev@careerfoundry.com",
+      "self": true
+    },
+    "start": {
+      "dateTime": "2022-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "end": {
+      "dateTime": "2022-06-30T10:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "48m09le389vijbqfs2mcr6i87q",
+    "originalStartTime": {
+      "dateTime": "2022-06-30T09:00:00+02:00",
+      "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "48m09le389vijbqfs2mcr6i87q@google.com",
+    "sequence": 1,
+    "hangoutLink": "https://meet.google.com/wdc-wutt-tji",
+    "conferenceData": {
+      "entryPoints": [
+        {
+          "entryPointType": "video",
+          "uri": "https://meet.google.com/wdc-wutt-tji",
+          "label": "meet.google.com/wdc-wutt-tji"
+        },
+        {
+          "entryPointType": "more",
+          "uri": "https://tel.meet/wdc-wutt-tji?pin=3796108774207",
+          "pin": "3796108774207"
+        },
+        {
+          "regionCode": "DE",
+          "entryPointType": "phone",
+          "uri": "tel:+49-40-8081618974",
+          "label": "+49 40 8081618974",
+          "pin": "170634768"
+        }
+      ],
+      "conferenceSolution": {
+        "key": {
+          "type": "hangoutsMeet"
+        },
+        "name": "Google Meet",
+        "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+      },
+      "conferenceId": "wdc-wutt-tji"
+    },
+    "reminders": {
+      "useDefault": true
+    },
+    "eventType": "default"
   }
 ];//end mockData array
 
